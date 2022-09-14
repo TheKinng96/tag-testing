4d48852ffbd5182765270d0a889ec9ecd3f5e142
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 08:28:24 +0900

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,33 +1,19 @@
 (function () {
   let temp = document.createElement('div')
-  temp.innerHTML = `<script type="text/javascript" async>
-          //<![CDATA[
-            var yahoo_conversion_id = 1001260891;
-            var yahoo_conversion_label = "swEjCI_M5doDEJqp5_Io";
-            var yahoo_conversion_value = window.Shopify.checkout.subtotal_price;
-          // ]]>
-        </script>
-        <script type="text/javascript" src="https://s.yimg.jp/images/listing/tool/cv/conversion.js">
-        </script>
-        <script type="text/javascript" async>
-            (function () {
-                let noscript = document.createElement('noscript')
-                let div = document.createElement('div')
-                div.setAttribute("style", "display:inline;")
-                
-                let img = document.createElement('img')
-                const head = document.head
-                img.src = 'https://b91.yahoo.co.jp/pagead/conversion/1001260891/?value=' + window.Shopify.checkout.subtotal_price + '&label=swEjCI_M5doDEJqp5_Io&guid=ON&script=0&disvt=true'
-                img.width = '1'
-                img.height = '1'
-                img.border = "0"
-                
-                div.appendChild(img)
-                noscript.appendChild(div)
-                
-                document.getElementsByTagName("head")[0].appendChild(noscript)
-            })()
-        </script>`
+  temp.innerHTML = `<script async src="https://s.yimg.jp/images/listing/tool/cv/ytag.js"></script>
+    <script>
+      ytag({"type":"ycl_cookie"});
+    </script>
+    <script async>
+    ytag({
+      "type": "yss_conversion",
+      "config": {
+        "yahoo_conversion_id": "1001259409",
+        "yahoo_conversion_label": "lEJMCLn98dgDEOOcq-8o",
+        "yahoo_conversion_value": window.Shopify.checkout.subtotal_price.toString()
+      }
+    });
+    </script>`
   let head = document.head
   
   Array.prototype.forEach.call(temp.children, function(item) {
