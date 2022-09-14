8a28ad9a4b345a14f630a2264de7ae95bcb763f4
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-14 17:06:20 +0900

    Update tag.js

--- tag.js
+++ tag.js
@@ -11,13 +11,21 @@
         </script>
         <script type="text/javascript" async>
             (function () {
+                let noscript = document.createElement('noscript')
+                let div = document.createElement('div')
+                div.setAttribute("style", "display:inline;")
+                
                 let img = document.createElement('img')
                 const head = document.head
                 img.src = 'https://b91.yahoo.co.jp/pagead/conversion/1001260891/?value=' + window.Shopify.checkout.subtotal_price + '&label=swEjCI_M5doDEJqp5_Io&guid=ON&script=0&disvt=true'
                 img.width = '1'
                 img.height = '1'
                 img.border = "0"
-                document.getElementsByTagName("head")[0].appendChild(img)
+                
+                div.appendChild(img)
+                noscript.appendChild(div)
+                
+                document.getElementsByTagName("head")[0].appendChild(noscript)
             })()
         </script>`
   let head = document.head
