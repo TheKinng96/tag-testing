d737c3f549d98dc025c5f56d7bf23a9005a56828
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 11:47:16 +0900

    Update search.js

--- search.js
+++ search.js
@@ -24,42 +24,32 @@
          ],
        },
      }
-     if (${window.ShopifyAnalytics.meta.page.pageType === 'product'}) {
-        let test = ${[window.ShopifyAnalytics.meta.product].map(item => {return { id: item.id, category_id: item.type, price: item.variants[0].price / 100, quantity: '' }})}
-         console.log(test)
-         ytag({
-           "type": "yjad_retargeting",
-           "config": {
-             "yahoo_retargeting_id": "8YRDX0ZJLS",
-             "yahoo_retargeting_label": "",
-             "yahoo_retargeting_page_type": 'detail',
-             "yahoo_retargeting_items": [
-               {
-                   item_id: ${window.ShopifyAnalytics.meta.product.id},
-                   category_id: "",
-                   price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
-                   quantity: ""
-               },
-             ],
-           },
-         });
-         ytag({
-           "type": "yjad_retargeting",
-           "config": {
-             "yahoo_retargeting_id": "8YRDX0ZJLS",
-             "yahoo_retargeting_label": "",
-             "yahoo_retargeting_page_type": 'detail',
-             "yahoo_retargeting_items": [
-               {
-                   item_id: "${window.ShopifyAnalytics.meta.product.id}",
-                   category_id: "${window.ShopifyAnalytics.meta.product.type}",
-                   price: "${window.ShopifyAnalytics.meta.product.variants[0].price / 100}",
-                   quantity: ""
-               },
-             ],
-           },
-         });
-       }
+     
+     switch (${window.ShopifyAnalytics.meta.page.pageType}) {
+      case 'product':
+        details.yahoo_retargeting_page_type = 'detail';
+        details.yahoo_retargeting_items = [{
+          item_id: ${window.ShopifyAnalytics.meta.product.id},
+          category_id: "",
+          price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
+          quantity: ""
+        }];
+        break;
+      case 'collection':
+        details.yahoo_retargeting_page_type = 'category';
+        details.yahoo_retargeting_items = ${window.ShopifyAnalytics.meta.products.map(item => {return { id: item.id, category_id: item.type, price: item.variants[0].price / 100, quantity: '' }})}
+        break;
+      case 'searchresults':
+        details.yahoo_retargeting_page_type = 'search';
+        break;
+      default:
+        details.yahoo_retargeting_page_type = 'home';
+        break;
+    }
+ 
+       console.log(details);
+       ytag(details);
+      }
     </script>`
   
   let head = document.head;
