75176d25df39d1314c467f6f6c681af46276401b
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 10:59:51 +0900

    Update search.js

--- search.js
+++ search.js
@@ -8,31 +8,32 @@
       function ytag() { yjDataLayer.push(arguments); }
       ytag({"type":"ycl_cookie"});
 
-//       if (${window.ShopifyAnalytics.meta.page.pageType} === 'product') {
-//         ytag({
-//           "type":"yjad_retargeting",
-//           "config":{
-//             "yahoo_retargeting_id": "8YRDX0ZJLS",
-//             "yahoo_retargeting_label": "",
-//             "yahoo_retargeting_page_type": 'detail',
-//             "yahoo_retargeting_items":[
-//               {
-//                   item_id: ${window.ShopifyAnalytics.meta.product.id},
-//                   category_id: ${window.ShopifyAnalytics.meta.product.type},
-//                   price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
-//                   quantity: ""
-//               }
-//             ]
-//           }
-//         });
-//       }
+       if (${window.ShopifyAnalytics.meta.page.pageType} === 'product') {
+         console.log('here')
+         ytag({
+           "type": "yjad_retargeting",
+           "config": {
+             "yahoo_retargeting_id": "8YRDX0ZJLS",
+             "yahoo_retargeting_label": "",
+             "yahoo_retargeting_page_type": 'detail',
+             "yahoo_retargeting_items": [
+               {
+                   item_id: "${window.ShopifyAnalytics.meta.product.id}",
+                   category_id: "${window.ShopifyAnalytics.meta.product.type}",
+                   price: "${window.ShopifyAnalytics.meta.product.variants[0].price / 100}",
+                   quantity: ""
+               },
+             ],
+           },
+         });
+       }
     </script>`
   
-  let head = document.head
+  let head = document.head;
   
   Array.prototype.forEach.call(temp.children, function(item) {
-      let element = document.createElement(item.tagName)
-      element.innerHTML = item.innerHTML
-      document.getElementsByTagName( "head" )[0].appendChild(element)
+      let element = document.createElement(item.tagName);
+      element.innerHTML = item.innerHTML;
+      document.getElementsByTagName( "head" )[0].appendChild(element);
   });
 })()
