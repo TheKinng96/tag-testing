249b230fcb145b7985d45b6d4189edde1eaefda4
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 09:55:41 +0900

    Update search.js

--- search.js
+++ search.js
@@ -2,46 +2,47 @@
   let temp = document.createElement('div')
   temp.innerHTML = `
     <script async src="https://s.yimg.jp/images/listing/tool/cv/ytag.js"></script>
-    <script>
-    window.yjDataLayer = window.yjDataLayer || [];
-    
-    function ytag() { yjDataLayer.push(arguments); }
-    ytag({"type":"ycl_cookie"});
-    
-    let retargetingDetail = {
-      "type":"yjad_retargeting",
-      "config":{
-        "yahoo_retargeting_id": "8YRDX0ZJLS",
-        "yahoo_retargeting_label": "",
-        "yahoo_retargeting_page_type": "",
-        "yahoo_retargeting_items":[
-          {item_id: '', category_id: '', price: '', quantity: ''}
-        ]
-      }
-    };
-    
-    let yahooCurrentUrl = location.href;
-    if (yahooCurrentUrl.match(/products/)) {
-        retargetingDetail.config.yahoo_retargeting_page_type = 'detail'
-        retargetingDetail.config.yahoo_retargeting_items = [
-            {
-                item_id: ${window.ShopifyAnalytics.meta.product.id},
-                category_id: '',
-                price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
-                quantity: ''
-            }
-        ]
-    } else if (yahooCurrentUrl.match(/collections/)) {
-        targetingObject.config.yahoo_retargeting_page_type = 'category'
-    } else if (yahooCurrentUrl.match(/cart/)) {
-        targetingObject.config.yahoo_retargeting_page_type = 'cart'
-    } else if (yahooCurrentUrl.match(/search/)) {
-        targetingObject.config.yahoo_retargeting_page_type = 'search'
-    } else {
-        targetingObject.config.yahoo_retargeting_page_type = 'home'
-    }
+    <script type="text/javascript" defer>
+      window.yjDataLayer = window.yjDataLayer || [];
 
-    ytag(retargetingDetail);
+      function ytag() { yjDataLayer.push(arguments); }
+      ytag({"type":"ycl_cookie"});
+
+      let retargetingDetail = {
+        "type":"yjad_retargeting",
+        "config":{
+          "yahoo_retargeting_id": "8YRDX0ZJLS",
+          "yahoo_retargeting_label": "",
+          "yahoo_retargeting_page_type": "",
+          "yahoo_retargeting_items":[
+            {item_id: '', category_id: '', price: '', quantity: ''}
+          ]
+        }
+      };
+
+      let yahooCurrentUrl = location.href;
+      console.log(yahooCurrentUrl);
+      if (yahooCurrentUrl.match(/products/)) {
+          retargetingDetail.config.yahoo_retargeting_page_type = 'detail'
+          retargetingDetail.config.yahoo_retargeting_items = [
+              {
+                  item_id: ${window.ShopifyAnalytics.meta.product.id},
+                  category_id: '',
+                  price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
+                  quantity: ''
+              }
+          ]
+      } else if (yahooCurrentUrl.match(/collections/)) {
+          targetingObject.config.yahoo_retargeting_page_type = 'category'
+      } else if (yahooCurrentUrl.match(/cart/)) {
+          targetingObject.config.yahoo_retargeting_page_type = 'cart'
+      } else if (yahooCurrentUrl.match(/search/)) {
+          targetingObject.config.yahoo_retargeting_page_type = 'search'
+      } else {
+          targetingObject.config.yahoo_retargeting_page_type = 'home'
+      }
+
+      ytag(retargetingDetail);
     </script>`
   
   let head = document.head
