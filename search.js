c11efb42e293b0a2b18a56b29718c8344e218e11
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 12:41:01 +0900

    Update search.js

--- search.js
+++ search.js
@@ -16,14 +16,36 @@
        "type": "yjad_retargeting",
        "config": {
          "yahoo_retargeting_id": "8YRDX0ZJLS",
-         "yahoo_retargeting_label": "",
+         "yahoo_retargeting_label": "","yahoo_retargeting_page_type": "home",
+         "yahoo_retargeting_items": [
+           {
+               item_id: "",
+               category_id: "",
+               price: "",
+               quantity: ""
+           },
+         ],
        },
      }
      
       if (window.ShopifyAnalytics.meta.page.pageType === 'product') {
         details.yahoo_retargeting_page_type = 'detail';
+        details.yahoo_retargeting_items = [{
+          item_id: window.ShopifyAnalytics.meta.product.id,
+          category_id: window.ShopifyAnalytics.meta.product.type,
+          price: window.ShopifyAnalytics.meta.product.variants[0].price / 100,
+          quantity: ""
+        }];
       } else if (window.ShopifyAnalytics.meta.page.pageType === 'collection') {
         details.yahoo_retargeting_page_type = 'category';
+        details.yahoo_retargeting_items = window.ShopifyAnalytics.meta.products.map(item => {
+          return { 
+            id: item.id,
+            category_id: item.type,
+            price: item.variants[0].price / 100,
+            quantity: '' 
+           }
+         })
       } else if (window.ShopifyAnalytics.meta.page.pageType === 'searchresults') {
         details.yahoo_retargeting_page_type = 'search';
       }
