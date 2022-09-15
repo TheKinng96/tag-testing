468c4d98d9566c49c5a1d0756d9fbf2c6db96b2d
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 10:56:54 +0900

    Update search.js

--- search.js
+++ search.js
@@ -8,25 +8,24 @@
       function ytag() { yjDataLayer.push(arguments); }
       ytag({"type":"ycl_cookie"});
 
-      let currentPage = location.href;
-      if (${window.ShopifyAnalytics.meta.page.pageType} === 'product') {
-          ytag({
-          "type":"yjad_retargeting",
-          "config":{
-            "yahoo_retargeting_id": "8YRDX0ZJLS",
-            "yahoo_retargeting_label": "",
-            "yahoo_retargeting_page_type": 'detail',
-            "yahoo_retargeting_items":[
-              {
-                  item_id: ${window.ShopifyAnalytics.meta.product.id},
-                  category_id: ${window.ShopifyAnalytics.meta.product.type},
-                  price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
-                  quantity: ""
-              }
-            ]
-          }
-        });
-      }
+//       if (${window.ShopifyAnalytics.meta.page.pageType} === 'product') {
+//         ytag({
+//           "type":"yjad_retargeting",
+//           "config":{
+//             "yahoo_retargeting_id": "8YRDX0ZJLS",
+//             "yahoo_retargeting_label": "",
+//             "yahoo_retargeting_page_type": 'detail',
+//             "yahoo_retargeting_items":[
+//               {
+//                   item_id: ${window.ShopifyAnalytics.meta.product.id},
+//                   category_id: ${window.ShopifyAnalytics.meta.product.type},
+//                   price: ${window.ShopifyAnalytics.meta.product.variants[0].price / 100},
+//                   quantity: ""
+//               }
+//             ]
+//           }
+//         });
+//       }
     </script>`
   
   let head = document.head
