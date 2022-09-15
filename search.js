cf76ffa8c50bf27d042ac3827b47b4aedbaae265
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2022-09-15 12:23:33 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,16 @@
 (function () {
-  let temp = document.createElement('div')
-  temp.innerHTML = `<script async src="https://s.yimg.jp/images/listing/tool/cv/ytag.js"></script>`
+  let temp = document.createElement('script')
+  temp.setAttribute('async', '');
+  temp.setAttribute(
+    'src',
+    'https://s.yimg.jp/images/listing/tool/cv/ytag.js',
+  );
+  
   let script = document.createElement('script');
   script.setAttribute('defer', '');
   script.innerHTML = `
     window.yjDataLayer = window.yjDataLayer || [];
-
-      function ytag() { yjDataLayer.push(arguments); }
+    function ytag() { yjDataLayer.push(arguments); }
       ytag({"type":"ycl_cookie"});
       
       let details = {
@@ -14,7 +18,7 @@
        "config": {
          "yahoo_retargeting_id": "8YRDX0ZJLS",
          "yahoo_retargeting_label": "",
-         "yahoo_retargeting_page_type": "detail",
+         "yahoo_retargeting_page_type": "home",
          "yahoo_retargeting_items": [
            {
                item_id: "",
@@ -26,7 +30,6 @@
        },
      }
      
-     console.log(window.ShopifyAnalytics.meta.page.pageType);
       if (window.ShopifyAnalytics.meta.page.pageType === 'product') {
         details.yahoo_retargeting_page_type = 'detail';
         details.yahoo_retargeting_items = [{
@@ -37,16 +40,21 @@
         }];
       } else if (window.ShopifyAnalytics.meta.page.pageType === 'collection') {
         details.yahoo_retargeting_page_type = 'category';
-        details.yahoo_retargeting_items = window.ShopifyAnalytics.meta.products.map(item => {return { id: item.id, category_id: item.type, price: item.variants[0].price / 100, quantity: '' }})
+        details.yahoo_retargeting_items = window.ShopifyAnalytics.meta.products.map(item => {
+          return { 
+            id: item.id,
+            category_id: item.type,
+            price: item.variants[0].price / 100,
+            quantity: '' 
+           }
+         })
       } else if (window.ShopifyAnalytics.meta.page.pageType === 'searchresults') {
         details.yahoo_retargeting_page_type = 'search';
-      } else {
-        details.yahoo_retargeting_page_type = 'home';
       }
  
        console.log(details);
        ytag(details);
-`
+    `;
   
   let head = document.head;
   head.appendChild(script);
